3034af10cde4317808a3c59490de25ea9067bdd1
Imad Ali <imadmali@gmail.com>
2016-02-10 19:59:36 -0500

    updated stan_tutorial.R

--- stan_tutorial.R
+++ stan_tutorial.R
@@ -23,6 +23,12 @@ names(eightschools) <- c("school","te","se")
 ### PARAMETER RECOVERY ###
 ##########################
 
+# generate 1000 independent variables from the poission distribution
+# with paramter lambda=7. generate 1000 independent variables from
+# the normal distribution with mean 0 and standard deviation 1. 
+# specify the following model: y = 0.3 + 0.8*x1 + 12*x2 + e where
+# e ~ uniform(0,1). Recover the parameters.
+
 #################################
 ### LINEAR REGRESSION EXAMPLE ###
 #################################
@@ -37,6 +43,13 @@ names(eightschools) <- c("school","te","se")
 # to compare your results. Note that your parameter estimates won't be
 # identical since Stan is running a stochastic algorithm.
 
+#######################################
+### HIERARCHICAL REGRESSION EXAMPLE ###
+#######################################
+
+# split the cornwell and rupert data according to gender. specify separate
+# models but apply a prior that controlls the degree of pooling. 
+
 ###################################
 ### LOGISTIC REGRESSION EXAMPLE ###
 ###################################
@@ -48,9 +61,3 @@ names(eightschools) <- c("school","te","se")
 ###########################
 
 # read in the longley data.
-
-################################################
-### HIERARCHICAL REGRESSION MODELING EXAMPLE ###
-################################################
-
-# read in the baltagi data.
